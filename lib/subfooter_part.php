4f18a56fd7be45556a36e8bcf72db10b2f01652c
laughing_cat <lideainfo01@gmail.com>
2022-11-24 15:21:45 +0900

    追加ファイルアップロード

---
+++ lib/subfooter_part.php
@@ -0,0 +1,21 @@
+<div id="back_wrap">
+	<form method="post" action="options.php" enctype="multipart/form-data" encoding="multipart/form-data">
+    <?php
+    settings_fields('custom-menu-group');
+    do_settings_sections('custom-menu-group'); ?>
+
+	<div class="metabox-holder">
+      <div class="postbox bg">
+        <h3 class='hndle'><span class="title">サブフッター設定</span></h3>
+        <div class="inside">
+          <div class="main">
+            <p class="setting_description">サブフッターを設定すると、メニュー一覧ページ、個別商品ページに自動で差し込まれます。メニュー内でユーザー回遊が起こりやすく、サイト滞在時間が伸びますので設定する事を推奨します。</p>
+            <h4>テキスト</h4>
+            <p><input type="text" id="text" name="text" value="<?php echo get_option('text'); ?>"></p>
+          </div>
+        </div>
+      </div>
+	</div>
+	<?php submit_button(); ?>
+	</form>
+</div>
