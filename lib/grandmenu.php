6492f5036c3a50412da5b49509b8cd44540ebc06
laughing_cat <lideainfo01@gmail.com>
2023-10-20 15:14:06 +0900

    配列に戻す処理を実装

--- lib/grandmenu.php
+++ lib/grandmenu.php
@@ -4,22 +4,21 @@
 			<?php
 			  require_once(dirname(dirname(dirname(dirname(dirname( __FILE__ ))))) . '/wp-load.php' );
 			  global $wpdb;
-			  $query = "SELECT grandmenu_img_url,  grandmenu_title,  FROM wp_narukami_content_maker;";
-			  $rows = $wpdb->get_results($query);
+			  $query = "SELECT grandmenu_img_url,  grandmenu_title FROM wp_narukami_content_maker;";
+			  $rows = $wpdb->get_results($query,OBJECT);
+			  var_dump($query);
 			  foreach($rows as $row) {
 				 $grandmenu_img_url = $row->grandmenu_img_url;
-				 $grandmenu_title = $row->grandmenu_title; 
+				 $grandmenu_title = $row->grandmenu_title;
 			  };
 			  ?>
 			<div class="grandmenu-wrap" >
 				<div class="grandmenu-title-wrap">
 					<ul>
 						<?php
-						foreach( $grandmenu_img_url as $gm_url){
-							echo "<li>" . $gm_url . "</li>";
-						}
-						foreach( $grandmenu_title as $gm_title ){
-							echo "<li>" . $gm_title . "</li>";
+						var_dump($grandmenu_title);
+						foreach( $grandmenu_title_dec as $grandmenu_title_decs){
+							echo "<li>". $grandmenu_title_decs . "</li>";
 						}
 						;?>
 					
@@ -42,7 +41,7 @@
   	generate_upload_image_tag('grandmenu_img_url', $grandmenu_img_bg_url);
 	?>
 	<h4>コンセプトタイトルを入力してください。</h4>
-	<input type="text" name="grandmenu_title[test1]" class="img-setect-url" value=
+	<input type="text" name="grandmenu_title[]" class="img-setect-url" value=
 		   <?php
 		   if( isset($_POST['grandmenu_title'])){
 			   echo $_POST['grandmenu_title'];
@@ -51,9 +50,9 @@
 		   }
 			   ;?>
 		   >
-		<input type="text" name="grandmenu_title[test2]" class="img-setect-url">
-		<input type="text" name="grandmenu_title[test3]" class="img-setect-url">
-		<input type="text" name="grandmenu_title[test4]" class="img-setect-url">
+		<input type="text" name="grandmenu_title[]" class="img-setect-url">
+		<input type="text" name="grandmenu_title[]" class="img-setect-url">
+		<input type="text" name="grandmenu_title[]" class="img-setect-url">
 		<?php 
 		$grandmenu_title_json = $_POST['grandmenu_title'];
 		echo json_encode($grandmenu_title_json, JSON_UNESCAPED_UNICODE);
