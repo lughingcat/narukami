4677f71fc6a2805ae0577d256385c8527ccd8af0
laughing_cat <lideainfo01@gmail.com>
2024-01-09 14:24:24 +0900

    store_info作成

--- functions.php
+++ functions.php
@@ -387,7 +387,7 @@ function ranking_db_farst_insert_data(){
 	$column_3_title_path = "SAMPLE&nbsp;PRIMARY";
 	$column_2_sec_title_path = "SAMPLE&nbsp;SECONDERY";
 	$column_3_sec_title_path = "SAMPLE&nbsp;SECONDERY";
-	$column_3_third_title_path = "SAMPLE&nbsp;THIRDERY";
+	$column_3_third_title_path = "SAMPLE&nbsp;TERTIARY";
 	$gm_primary_title_path = "GRAND&nbsp;MENU";
 	$concept_content_sample =<<< EOM
 		親譲りの無鉄砲で小供の時から損ばかりしている。小学校に居る時分学校の二階から飛び降りて一週間ほど腰を抜かした事がある。なぜそんな無闇をしたと聞く人があるかも知れぬ。別段深い理由でもない。新築の二階から首を出していたら、同級生の一人が冗談に、いくら威張っても、そこから飛び降りる事は出来まい。弱虫やーい。と囃したからである。小使に負ぶさって帰って来た時、おやじが大きな眼をして二階ぐらいから飛び降りて腰を抜かす奴があるかと云ったから、この次は抜かさずに飛んで見せますと答えた。（青空文庫より）
