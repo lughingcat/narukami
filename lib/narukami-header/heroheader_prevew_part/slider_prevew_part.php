4a497ae267d33f40882ba8f2ec39bb3e3fbaa52c
lughingcat <lideainfo01@gmail.com>
2024-08-05 13:05:36 +0900

    slick

--- lib/narukami-header/heroheader_prevew_part/slider_prevew_part.php
+++ lib/narukami-header/heroheader_prevew_part/slider_prevew_part.php
@@ -1,19 +1,52 @@
+<?php
+	$slider_img_link = array(
+		'https://nousondiner.com/wp-content/uploads/2024/07/beef.jpg',
+		'https://nousondiner.com/wp-content/uploads/2024/07/salmon.jpg',
+		'https://nousondiner.com/wp-content/uploads/2024/07/unadon.jpg'
+	);
+	$slider_item_title = array(
+		'Slide 1',
+		'Slide 2',
+		'Slide 3'
+	);
+	// 連想配列を作成
+	$slider_items_Array = array();
+			
+	// $i_global_title_arrayが配列であるかを確認
+	if (is_array($slider_img_link)) {
+		// 各変数が配列であるかを確認
+		if (is_array($slider_item_title)) {
+			for ($s_num = 0; $s_num < count($slider_item_title); $s_num++) {
+				// 配列のインデックスが存在するかを確認
+				if (isset($slider_item_title[$s_num])) {
+					$slider_items_Array[$slider_item_title[$s_num]] = array(
+						'title' => $slider_item_title[$s_num],
+						'url' => $slider_img_link[$s_num]
+					);
+				} else {
+				//タイトル, URL,　エラーハンドリング
+				}
+			}
+		} else {
+			//arrayに対するエラーハンドリング
+		}
+	} else {
+		//全体arrayに対するエラーハンドリング
+	}
+?>
+aaa
 <div class="heroheader-prevew-slider-all-wrap">
-		<div class="heroheader-slider-wrap">
-			<div>
-				<img src="https://nousondiner.com/wp-content/uploads/2024/07/beef.jpg" alt="Slide 1">
-				<div class="back-shadow"></div>
-				<p>Slide 1</p>
-			</div>
-    		<div>
-				<img src="https://nousondiner.com/wp-content/uploads/2024/07/salmon.jpg" alt="Slide 2">
-				<div class="back-shadow"></div>
-				<p>Slide 2</p>
-			</div>
-    		<div>
-				<img src="https://nousondiner.com/wp-content/uploads/2024/07/unadon.jpg" alt="Slide 3">
-				<div class="back-shadow"></div>
-				<p>Slide 3</p>
-			</div>
-		</div><!--heroheader-slider-wrap-end-->
-</div><!--heroheader-prevew-video-all-wrap-end-->
+    <?php
+    foreach($slider_items_Array as $key => $item){
+        echo '<div class="heroheader-slider-wrap">';
+        echo '<div>';
+        echo '<div>';
+        echo '<img src="' . $item['url'] . '" alt="Slide 1">';
+        echo '<div class="back-shadow"></div>';
+        echo '<p>' . $item['title'] . '</p>';
+        echo '</div>';
+        echo '</div>';
+        echo '</div>'; // 閉じタグを追加
+    }
+    ?>
+</div><!--heroheader-prevew-slider-all-wrap-end-->
