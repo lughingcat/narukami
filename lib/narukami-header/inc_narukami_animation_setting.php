d5bae69a75dc006cd2a4e872b26766833641e39e
lughingcat <lideainfo01@gmail.com>
2024-08-15 19:09:59 +0900

    19:10

--- lib/narukami-header/inc_narukami_animation_setting.php
+++ lib/narukami-header/inc_narukami_animation_setting.php
@@ -1,7 +1,7 @@
 
 <div class="anime-set-prevew-wrap">
 	<p>アニメーションテスト</p>
-	<div class="animetion-prewrap stretch-shrink">
+	<div class="animetion-prewrap">
 	</div>
 </div>
 <button id="animateButton" type="button">アニメーション開始</button>
