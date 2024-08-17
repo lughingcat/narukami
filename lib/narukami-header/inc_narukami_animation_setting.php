eac2f362d70158895b37fa90ba9ec1101d88bb5b
laughing_cat <lideainfo01@gmail.com>
2024-08-17 19:11:36 +0900

    19:11

--- lib/narukami-header/inc_narukami_animation_setting.php
+++ lib/narukami-header/inc_narukami_animation_setting.php
@@ -2,7 +2,9 @@
 	<div class="anime-set-prevew-wrap">
 		<p>アニメーションテスト</p>
 	</div>
-	<div class="animetion-prewrap"></div>
+	<div class="animetion-prewrap">
+		<p>ここにタイトルが入ります</p>
+	</div>
 </div>
 <button id="right-to-left-btn" class="load-anime-prevew-btn" type="button">右スライド</button>
 <button id="curtain-btn" class="load-anime-prevew-btn" type="button">カーテン</button>
