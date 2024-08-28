f7da70b7e10d2362f8bcb1ff2bd64c3ba3d00bbb
laughing_cat <lideainfo01@gmail.com>
2024-08-28 15:21:16 +0900

    15:21

--- custom-header-prevew-temp.php
+++ custom-header-prevew-temp.php
@@ -5,14 +5,16 @@
     <title><?php echo get_bloginfo('name'); ?></title>
     <?php wp_head(); ?>
 </head>
-<body>
+<body style="position: relative;">
 	 <?php
 	if ($_SERVER['REQUEST_METHOD'] === 'POST') {
         // フォームデータを取得して表示
         $headerformData = $_POST;
-		//var_dump($headerformData);
 		if($headerformData['header-display-setting'] === 'display_on'){
 			include(get_template_directory() . '/front-inc-header/inc-header-part.php');
+		}
+		if($headerformData['heorheader-type'] === 'still_img'){
+			include(get_template_directory() . '/front-inc-header/inc-HH-still-img-part.php');
 		}
 	}
     ?>
---
+++ front-inc-header/inc-HH-still-img-part.php
@@ -0,0 +1,20 @@
+<?php
+//静止画変数
+$i_heroheader_stillImg = sanitize_option_value($_POST['hh-still-img']);
+$i_heroheader_stillTitle = sanitize_option_value($_POST['hh-still-title']);
+$i_hh_still_title_color = sanitize_option_value($_POST['hh-titleTextColor']);
+$i_hh_still_shadow_color = sanitize_option_value($_POST['hh-titleShadowColor']);
+?>
+<div class="heroheader-prevew-all-wrap">
+	<div class="heroheader-back-wrap">
+		<div class="heroheader-rogo-wrap"
+			 style="background-image: url('<?php echo $i_heroheader_stillImg; ?>');">
+				<div class="heroheader-title-wrap">
+					<p class="heroheader-title" 
+					   style="color: <?php echo $i_hh_still_title_color; ?>; text-shadow: 1px 1px 10px <?php echo $i_hh_still_shadow_color; ?>;">
+						<?php echo $i_heroheader_stillTitle; ?>
+					</p>
+				</div>
+		</div>
+	</div><!--heroheader-back-wrap-end-->
+</div><!--heroheader-prevew-all-wrap-end-->
