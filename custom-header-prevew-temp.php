0092eb03f9b9e1f6c2973a1a3e50d75d243b9251
laughing_cat <lideainfo01@gmail.com>
2024-09-11 14:58:43 +0900

    9/11:14:59　トップページビルダーをdbから読み込ませる処理をコーディング中

--- custom-header-prevew-temp.php
+++ custom-header-prevew-temp.php
@@ -50,7 +50,35 @@
 			$concept_bg_img_url = $row['concept_bg_img_url'];
 			include(get_template_directory() . '/front-inc/front_concept.php');
 		}
-	}
+		if($cmaker === 'grandmenu'){
+			$row = $wpdb->get_row(
+    			$wpdb->prepare(
+        			"SELECT 
+					insert_ids, 
+					gm_primary_title, 
+					grandmenu_img_url,
+					grandmenu_title,
+					grandmenu_pagelink
+         			FROM $content_maker_table 
+					WHERE s_cmaker = %s",
+        			$cmaker
+    				),
+    			ARRAY_A
+				);
+			//json decode
+			$gm_title_array = json_decode($row['grandmenu_title'], true);
+			$gm_img_array = json_decode($row['grandmenu_img_url'], true);
+			$gm_pagelink_array = json_decode($row['grandmenu_pagelink'], true);
+			//変数セット
+			$insert_ids = $row['insert_ids'];
+			$gm_primary_title = $row['gm_primary_title'];
+			$grandmenu_img_url = $gm_img_array;
+			$grandmenu_title = $gm_title_array;
+			$grandmenu_pagelink = $gm_pagelink_array;
+			include(get_template_directory() . '/front-inc/front_grandmenu.php');
+		}
+		
+	}//foreach end
 	?>
 	<div style="height: 200px; background-color: black;"></div>
 	<?php include(get_template_directory() . '/lib/narukami-footer/footer.php');?>
