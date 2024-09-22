55c55afc4d3f966193b0859e5129e7b10c646bef
lughingcat <lideainfo01@gmail.com>
2024-09-22 11:48:14 +0900

    9/22:11:48 フォントファミリーを消す

--- lib/narukami-overall/over-all-setting.php
+++ lib/narukami-overall/over-all-setting.php
@@ -11,21 +11,11 @@
 			<form id="narukami-overall-form" method="post" name="narukami-overall-form" action="<?php echo esc_url(admin_url('admin-post.php')); ?>">
 				<input type="hidden" name="action" value="update_custom_option_overall" /><!--post_nonce_check.phpへ送信(フッター用)-->
 			<div class="inputForm notfound-page-wrap">
-				<h4>フォント設定</h4>
-				<p class="font-family-preview">下記で選択したフォントファミリーのプレビューが表示されます。</br>この文章が選択したフォントファミリーのビジュアルです。</br>選択したフォントファミリーはサイト全てのフォントに適応されます。</br>Font Test</p>
-		
-				<select id="font-f-select" name="over-all-fontfamily">
-  					<option value="Noto Sans JP">Noto Sans JP</option>
-  					<option value="Hiragino Kaku Gothic ProN">Hiragino Kaku Gothic ProN</option>
-  					<option value="Yu Gothic">Yu Gothic</option>
-  					<option value="Meiryo">Meiryo</option>
-				</select>
-		
 				<h4>背景画像設定</h4>
 				<input type="text" name="banner-title" class="img-setect-url" value="<?php echo $i_banner_title; ?>" placeholder="告知タイトルを入力してください">
 				<h4>ファビコン設定</h4>
 				<?php
-  				generate_upload_image_single_tag('banner-img', $i_banner_img);
+  				generate_upload_image_single_tag('favicon-img', $i_favicon_img);
 				?>
 				<h4>バナーのリンク先ページのURLを入力してください。</h4>
 				<input type="text" name="banner-link" class="img-setect-url" value="<?php echo $i_banner_link; ?>" placeholder="URLを入力してください">
@@ -41,9 +31,5 @@
       </div><!--metabox-holder end-->
 </div><!--back_wrap end-->
 <script>
-document.getElementById('font-f-select').addEventListener('change', function() {
-        var selectedFont = this.value;
-        var previewElement = document.querySelector('.font-family-preview');
-		previewElement.style.fontFamily = selectedFont;
-    });
+
 </script>
