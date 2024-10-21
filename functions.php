f65e15775b4be0ad744bcfdb541a553d47036360
lughingcat <lideainfo01@gmail.com>
2024-10-21 12:57:35 +0900

    ttt

--- functions.php
+++ functions.php
@@ -675,6 +675,7 @@ function render_narukami_heading_block( $block_content, $block ) {
         if ( isset( $block['attrs']['narukami_subtitle'] ) 
     		&& ! empty( $block['attrs']['narukami_subtitle'] ) 
     		&& isset( $block['attrs']['className'] ) 
+    		&& isset( $block['attrs']['narukami_subtitleAlignment'] ) 
     		&& strpos( $block['attrs']['className'], 'narukami-hedding-style1' ) !== false ) {
             // サブタイトルのHTMLを作成
             $subtitle = '<p class="wp-block-heading narukami-subtitle" style="text-align: ' . esc_attr( $block['attrs']['narukami_subtitleAlignment'] ) . ';">' . esc_html( $block['attrs']['narukami_subtitle'] ) . '</p>';
